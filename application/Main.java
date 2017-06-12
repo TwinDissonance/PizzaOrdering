56612eef4ad60d0c44ba12a75fa873ebabf02b9d
LAPTOP-ALBERT\TwinDissonance <al.art.int@gmail.com>
2017-06-12 12:01:34 -0400

    Partially implemented Scene 2 as well as Window 2 and 3,

--- application/Main.java
+++ application/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -42,6 +43,8 @@ public class Main extends Application {
 	private void changeScene(Stage s){
 		s.hide();
 		Message.setText("");
+		WindowMessage.setText("");
+		WindowMessage2.setText("");
 		Scene scene;
 		if(CurrentScene == 1)
 			scene = loadScene1(s);
@@ -72,15 +75,15 @@ public class Main extends Application {
 		Submit_0.setOnAction(e -> {
 			//Validity Check, Employee IDs can be changed later
 			if(EmployeeID_0.getText().equals("1") || EmployeeID_0.getText().equals("2") || EmployeeID_0.getText().equals("3")){
-				Win1 = true;
+				ValidID_0 = true;
 				WindowMessage.setText("");
 				WindowMessage2.setText("");
 			}
 			else{
-				Win1 = false;
+				ValidID_0 = false;
 				WindowMessage2.setText("Invalid Employee ID");
 			}
-			if(Win1){
+			if(ValidID_0){
 				CurrentScene = 1;
 				changeScene(s);
 				w.hide();
@@ -92,8 +95,25 @@ public class Main extends Application {
 			loadDatabase();
 		});
 		Scene2_Button.setOnAction(e -> {
-			CurrentScene = 2;
-			changeScene(s);
+			if(DataEntered){
+				CurrentScene = 2;
+				changeScene(s);
+			}
+			else
+				Message.setText("No Database Loaded");
+		});
+		//Stage 2
+		NewCustomer_2.setOnAction(e -> {
+			Window2(w);
+		});
+		OldCustomer_2.setOnAction(e -> {
+			Window3(w);
+		});
+		Submit_22.setOnAction(e -> {
+			//To Do
+		});
+		Submit_23.setOnAction(e -> {
+			//To Do
 		});
 	}
 	
@@ -101,7 +121,7 @@ public class Main extends Application {
 	private Button Customer_0 = new Button("Customer");
 	private Button Employee_0 = new Button("Employee");
 	private Button Submit_0 = new Button("Submit");
-	private boolean Win1 = false;
+	private boolean ValidID_0 = false;
 	private TextField EmployeeID_0 = new TextField();
 	
 	//Start of program, Employee/Customer choice
@@ -109,8 +129,8 @@ public class Main extends Application {
 		Message.setText("Are you an employee or a customer?");
 		BorderPane bp = new BorderPane();
 		bp.setTop(Message);
-		bp.setLeft(Customer_0);
-		bp.setRight(Employee_0);
+		bp.setLeft(Customer_0); //Leads to Scene 2
+		bp.setRight(Employee_0); // Leads to Window 1 and then Scene 1
 		return new Scene(bp, 300, 41);
 	}
 	
@@ -120,15 +140,17 @@ public class Main extends Application {
 		BorderPane bp = new BorderPane();
 		bp.setTop(WindowMessage);
 		bp.setCenter(EmployeeID_0);
-		VBox vb = new VBox(WindowMessage2, Submit_0);
+		VBox vb = new VBox(WindowMessage2, Submit_0); //Submit leads to Scene 1
 		bp.setBottom(vb);
 		Scene Scene_1 = new Scene(bp);
 		w.setScene(Scene_1);
+		w.setTitle("Employee Sign In");
 		w.show();
 	}
 	
 	//Scene 1 variables
 	private Button loadData_1 = new Button("Select Database");
+	private boolean DataEntered = false;
 	
 	//Scene for loading Customer Database
 	private Scene loadScene1(Stage s){
@@ -149,16 +171,64 @@ public class Main extends Application {
 			}
 			Message.setText("Customer Database loaded.");
 			fileInput.close();
+			DataEntered = true;
 		} catch (Exception e) {
 			e.printStackTrace();
 			Message.setText("Invalid File Selected");
 		}
 	}
 	
-	//UNIMPLEMENTED
+	//Scene 2 variables
+	private Button NewCustomer_2 = new Button("New Customer");
+	private Button OldCustomer_2 = new Button("Old Customer");
+	private TextField CustomerPhone_2 = new TextField();
+	private TextField CustomerName_2 = new TextField();
+	private TextField CustomerAddress_2 = new TextField();
+	private TextField CustomerAccountType_2 = new TextField();
+	private TextField CustomerAddressInfo_2 = new TextField();
+	private Button Submit_22 = new Button("Submit");
+	private Button Submit_23 = new Button("Submit");
+	
+	//Scene to choose if Old or New Customer
 	private Scene loadScene2(Stage s){
-		Pane p = new Pane();
-		return new Scene(p);
+		BorderPane bp = new BorderPane();
+		bp.setLeft(NewCustomer_2);
+		bp.setRight(OldCustomer_2);
+		CustomerPhone_2.setPromptText("Enter your phone number.");
+		CustomerName_2.setPromptText("Enter your full name.");
+		CustomerAddress_2.setPromptText("Enter your address.");
+		CustomerAccountType_2.setPromptText("Enter your Charge Account type (Visa/Mastercard).");
+		CustomerAddressInfo_2.setPromptText("Enter any information to help find your address.");
+		return new Scene(bp, 200, 200);
+	}
+	
+	
+	//New Customer Window
+	private void Window2(Stage w){
+		WindowMessage.setText("Please enter your customer information.");
+		BorderPane bp = new BorderPane();
+		bp.setTop(WindowMessage);
+		GridPane gp = new GridPane();
+		bp.setCenter(gp);
+		//Text Field Setup
+		bp.setBottom(Submit_22);
+		Scene Scene_2 = new Scene(bp);
+		w.setScene(Scene_2);
+		w.show();
+	}
+	
+	//Old Customer Window
+	private void Window3(Stage w){
+		WindowMessage.setText("Please enter your customer information.");
+		BorderPane bp = new BorderPane();
+		bp.setTop(WindowMessage);
+		GridPane gp = new GridPane();
+		bp.setCenter(gp);
+		//Text Field Setup
+		bp.setBottom(Submit_23);
+		Scene Scene_2 = new Scene(bp);
+		w.setScene(Scene_2);
+		w.show();
 	}
 	
 	//UNIMPLEMENTED
